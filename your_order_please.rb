a756dbb56cd65b2624ec5f5ecb22cc1629913949
Sean Lenhart <theseanlenhart@gmail.com>
2021-06-28 14:56:42 -0400

    Finished Answer with Documentation

--- your_order_please.rb
+++ your_order_please.rb
@@ -2,11 +2,11 @@ def order(words)
   # Variables
   result = []
 
-  array = words.split.each do |word|   # For each word in words
-    word.each_char do |y|
-      if (48..57).include? y.ord
-        result[y.to_i - 1] = word
-      end
+  array = words.split.each do |word|  ## For each word in words...
+    word.each_char do |y|             ## For each char in that word...
+      if (48..57).include? y.ord      ## If that char is a number in ascii,
+        result[y.to_i - 1] = word     # place the word into result in the
+      end                             # position determined by the number
     end
   end
   result.join(" ")
