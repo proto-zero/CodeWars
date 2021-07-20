0fda4b252db061cb357bbc1cfa2d42bb267faeae
Sean Lenhart <theseanlenhart@gmail.com>
2021-07-20 10:58:36 -0400

    edit syntax of final return

--- extract_the_domain_name_from_a_url.rb
+++ extract_the_domain_name_from_a_url.rb
@@ -14,37 +14,37 @@ def domain_name(url)
     url = url.delete_prefix("www.")
   end
 
-  # Split on the dot
-  url = url.split(".")
+  # Split on the dot and return first part
+  url.split(".")[0]
 
   # Return first part
-  url[0]
+  # url[0]
 end
 
 
 # Old method
-def domain_name(url)
-  result = []
-  if url.start_with?("https")
-    url = url.split("https://")
-    url = url.join
-  elsif url.start_with? ("http")
-    url = url.split("http://")
-    url = url.join
-  end
-  if url.start_with?("www.")
-    url = url.split("www.")
-    url = url.join
-  end
-  url.each_char do |x|
-    result << x
-    if x == "."
-      break
-    end
-  end
-  result.pop
-  result.join
-end
+# def domain_name(url)
+#   result = []
+#   if url.start_with?("https")
+#     url = url.split("https://")
+#     url = url.join
+#   elsif url.start_with? ("http")
+#     url = url.split("http://")
+#     url = url.join
+#   end
+#   if url.start_with?("www.")
+#     url = url.split("www.")
+#     url = url.join
+#   end
+#   url.each_char do |x|
+#     result << x
+#     if x == "."
+#       break
+#     end
+#   end
+#   result.pop
+#   result.join
+# end
 
 puts domain_name("http://google.com")
 puts domain_name("www.xakep.ru")
