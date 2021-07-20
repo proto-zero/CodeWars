af9c2d645fd3a726252ba5a8301fbf39ef4d2e38
Sean Lenhart <theseanlenhart@gmail.com>
2021-07-20 10:54:03 -0400

    two complete methods

--- extract_the_domain_name_from_a_url.rb
+++ extract_the_domain_name_from_a_url.rb
@@ -1,12 +1,40 @@
 def domain_name(url)
+  # Variables
+  result = []
+
+  # Checking for http/https
+  if url.start_with?("https")
+    url = url.delete_prefix("https://")
+  elsif url.start_with? ("http")
+    url = url.delete_prefix("http://")
+  end
+
+  # Checking for www.
+  if url.start_with?("www.")
+    url = url.delete_prefix("www.")
+  end
+
+  # Split on the dot
+  url = url.split(".")
+
+  # Return first part
+  url[0]
+end
+
+
+# Old method
+def domain_name(url)
   result = []
   if url.start_with?("https")
-    url = url.delete("https://")
+    url = url.split("https://")
+    url = url.join
   elsif url.start_with? ("http")
-    url = url.delete("http://")
+    url = url.split("http://")
+    url = url.join
   end
   if url.start_with?("www.")
-    url = url.delete("www.")
+    url = url.split("www.")
+    url = url.join
   end
   url.each_char do |x|
     result << x
@@ -19,3 +47,4 @@ def domain_name(url)
 end
 
 puts domain_name("http://google.com")
+puts domain_name("www.xakep.ru")
