25f4f3609464c26182413a93ebf8af44e2186139
Sean Lenhart <theseanlenhart@gmail.com>
2021-08-27 10:43:04 -0400

    add documentation

--- range_extraction.rb
+++ range_extraction.rb
@@ -1,26 +1,28 @@
 def solution(list)
-  range = []
-  result = []
+  # Variables
+  range = []                     # range is a placeholder array for gathering a range of numbers
+  result = []                    # result is the final array
 
-  for i in 0...list.length
-    if list[i].next == list[i+1]
-      range << list[i]
-      range << list[i+1]
+  # For Loop
+  for i in 0...list.length       # For each number in list...
+    if list[i].next == list[i+1] # if that number's next increment equals the next number in the list...
+      range << list[i]           # shovel that number into the range array...
+      range << list[i+1]         # as well as the next number
     else
-      if !range.include? list[i]
-        result << list[i]
+      if !range.include? list[i] # If range doesn't include that number...
+        result << list[i]        # shovel that number directly into the result array
       end
-      if !range.empty?
-        if range.length > 2
-          result << "#{range.first}-#{range.last}"
-        else
-          result << range
+      if !range.empty?           # If the range is not empty...
+        if range.length > 2      # if range contains more than two numbers...
+          result << "#{range.first}-#{range.last}"  # shovel the first and last number into result
+        else                     # If range only has one or two numbers...
+          result << range        # shovel the number(s) into result without any extra formatting
         end
-        range = []
+        range = []               # Clear range of its previous value
       end
     end
   end
 
-  return result.join(",")
-
+  # Result
+  return result.join(",")        # result.join makes strings out of the numbers
 end
