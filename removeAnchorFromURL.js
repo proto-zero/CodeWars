66c2b67c6bd43b9ee63c95bb717c1ba019f68584
Sean Lenhart <theseanlenhart@gmail.com>
2021-09-09 13:04:26 -0400

    add removeAnchorFromURL.js

---
+++ removeAnchorFromURL.js
@@ -0,0 +1,16 @@
+function removeUrlAnchor(url){
+  // Variables
+  let result = ""
+
+  // For Loop
+  for (i = 0; i < url.length; i++){   // For each character in url...
+    if (url[i] != "#"){               // if that character isn't a #...
+      result += url[i];               // add that character to the result string
+    } else {                          // If that character is a #...
+      return result;                  // return result without adding anything more to it
+    }
+  }
+
+  // Result
+  return result;                      // If there is no # present, return the entire result
+}
